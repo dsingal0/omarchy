441d2bb2f5603eaf6eda5fd856143fda365afc3c
Dhruv Singal <dhruvsingalabc@gmail.com>
2025-07-01 11:52:39 -0700

    more cleanup

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,6 +1,5 @@
 paru -Syyu --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse \
   nautilus sushi gnome-calculator \
   brave-bin vlc \
--- migrations/1751135253.sh
+++
@@ -1,5 +0,0 @@
-if ! command -v bat &>/dev/null; then
-  # Add missing installation of bat
-  echo "Add missing installation of bat (used by the ff alias)"
-  paru -Syyu --noconfirm --needed bat
-fi
